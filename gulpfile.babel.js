39f7fe528580e21abe51f867f1e351f92aed164c
Joe-noh <goflb.jh@gmail.com>
2016-01-30 12:22:25 +0900

    rewrite gulpfile in es2015 syntax

---
+++ gulpfile.babel.js
@@ -0,0 +1,36 @@
+import browserify from 'browserify';
+import source from 'vinyl-source-stream';
+import glob from 'glob';
+import gulp from 'gulp';
+import gulpLoadPlugins from 'gulp-load-plugins';
+
+let gulpPlugins = gulpLoadPlugins({
+  pattern: ['gulp-*', 'gulp.*'],
+  replaceString: /\bgulp[\-.]/
+});
+
+let paths = {
+  root: './app/',
+  src:  './app/scripts/',
+  dist: './app/dist/'
+};
+
+gulp.task('browserify', () => {
+  browserify(`${paths.src}app.js`, {debug: true})
+  .bundle()
+  .pipe(source('app.js'))
+  .pipe(gulp.dest(paths.dist))
+  .pipe(gulpPlugins.connect.reload());
+});
+
+gulp.task('server', ['browserify'], () => {
+  gulpPlugins.connect.server({
+    root: './app',
+    livereload: true,
+  });
+});
+
+gulp.task('watch', () => {
+  gulp.start('server');
+  gulp.watch([`${paths.src}**/*.js`], ['browserify']);
+});
