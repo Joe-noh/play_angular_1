7ec5e01cab6eec6347ac1787dfb60b662c39fbd8
Joe-noh <honzawa.j@pepabo.com>
2016-02-16 10:09:47 +0900

    styling

--- app/scripts/services/note.js
+++ app/scripts/services/note.js
@@ -7,11 +7,10 @@ export default class NoteService {
       },
       {
         title: "今後の予定",
-        body: `
-        1. Angular2について調査
-        2. Angular2でなにか作ってみる
-        3. バックエンドとWebSocketで通信する
-        `
+        body:
+          '1. Angular2について調査\n' +
+          '2. Angular2でなにか作ってみる\n' +
+          '3. バックエンドとWebSocketで通信する'
       }
     ];
   }
